29c751f1f4d39da0cfd5143f1a38917c1d36d11b
Mike Ludemann <mike-ludemann@hotmail.de>
2020-01-14 14:22:45 +0100

    Setup complete - angular-interface

---
+++ src/app/interface/person.ts
@@ -0,0 +1,10 @@
+export interface Person {
+    firstname: string,
+    secondname?: string,
+    lastname: string,
+    street: string,
+    streetnumber: string,
+    postalcode: string
+    cities: string,
+    country: string
+}
